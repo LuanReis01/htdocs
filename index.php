02644a5ac6bf2aca5f32e7ae5868637f46de9b89
Luan <engluanreis@gmail.com>
2023-05-23 20:52:49 +0100

    Criação do novo repositório

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+$url_parts = explode("/", $_SERVER["REQUEST_URI"]);
+
+define("ENV", parse_ini_file(".env"));
+
+$controller = "home";
+
+$allowed_controllers = [
+    "cart", "checkout", "home", "login",
+    "logout", "productdetail", "products",
+    "register", "subcategories", "api"
+];
+
+if(!empty($url_parts[1])) {
+    $controller = $url_parts[1];
+}
+
+if(!empty($url_parts[2])) {
+    $id = $url_parts[2];
+}
+
+/* verificar se o controller pretendido está na nossa whitelist */
+if( !in_array($controller, $allowed_controllers) ) {
+    http_response_code(404);
+    die("Não encontrado");
+}
+
+require("controllers/" . $controller . ".php");
